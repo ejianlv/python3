1ec10041ad0d2123f18acce6cef8058d3447604b
Jian Lyu <jian.lyu@qq.com>
2018-08-08 11:23:30 -0700

    add 004

--- runoob003.py
+++ runoob003.py
@@ -98,5 +98,12 @@ pprint.pprint(data2)
 
 pkl_file.close()
 #----------------------------------------------------------------
-
+for arg in sys.argv[1:]:
+    try:
+        f = open(arg, 'r')
+    except IOError:
+        print('cannot open', arg)
+    else:
+        print(arg, 'has', len(f.readlines()), 'lines')
+        f.close()        
 #----------------------------------------------------------------
